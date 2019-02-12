7680656a099e34f972333b5ceed5c336a8c1391f
nuno1212s <nuno.neto.g@gmail.com>
2019-02-12 15:26:16 +0000

    Added log backstorage

--- ApplicationServer/src/main/java/com/nunoneto/loggers/Logger.java
+++ ApplicationServer/src/main/java/com/nunoneto/loggers/Logger.java
@@ -2,14 +2,14 @@ package com.nunoneto.loggers;
 
 import com.nunoneto.App;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Level;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 public class Logger {
 
@@ -28,6 +28,40 @@ public class Logger {
 
         outputFile = new File(dataFolder, "log.txt");
 
+        if (outputFile.exists()) {
+            //Output file already exists, zip the old log file and create a new one
+
+            File storageFile = new File(dataFolder, "log- " + getDate() + ".zip");
+
+            try (ZipOutputStream writeStream = new ZipOutputStream(new FileOutputStream(storageFile));
+                 //Read the old log file
+                    InputStream readStream = new FileInputStream(outputFile)) {
+
+                storageFile.createNewFile();
+
+                ZipEntry e = new ZipEntry("log.txt");
+
+                writeStream.putNextEntry(e);
+
+                int length;
+
+                //1KB read buffer
+                byte[] buffer = new byte[1024];
+
+                while ((length = readStream.read(buffer)) != 0) {
+
+                    writeStream.write(buffer, 0, length);
+
+                }
+
+                //Delete the contents of the output file
+                outputFile.delete();
+                outputFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         systemOut = System.out;
 
         try {
@@ -43,21 +77,14 @@ public class Logger {
 
     }
 
-    public void log(Object object)  {
+    public void log(Object object) {
         log(Level.INFO, object);
     }
 
     public void log(Level level, Object string) {
 
-        Calendar d = Calendar.getInstance();
 
-        Date time = d.getTime();
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
-        String format = dateFormat.format(time);
-
-        String logMessage = "[" + format + "] " + "[" + level.getName() + "] " + string.toString();
+        String logMessage = "[" + getDate() + "] " + "[" + level.getName() + "] " + string.toString();
 
         logOut.println(logMessage);
 
@@ -65,6 +92,16 @@ public class Logger {
 
         App.getAsync().submit(() -> logOut.flush());
 
+    }
+
+    private String getDate() {
+        Calendar d = Calendar.getInstance();
+
+        Date time = d.getTime();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+        return dateFormat.format(time);
     }
 
 }
